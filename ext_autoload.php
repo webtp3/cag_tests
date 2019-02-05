5b53c40feb09d0db6515435843c9ed5cbdbf44fd
tudy <email@thomasruta.de>
2019-02-05 04:07:30 -0800

    [fix] cag_tests path config for 9 compart & deployer releases

---
+++ ext_autoload.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+return (function () {
+    static $classLoader;
+    if ($classLoader) {
+        return $classLoader;
+    }
+
+    $typo3AutoLoadFile = realpath(($rootPath = dirname(__DIR__, 3)) . '/typo3') . '/../../current/build/vendor/autoload.php';
+    putenv('TYPO3_PATH_ROOT=' . $rootPath);
+    $classLoader = require $typo3AutoLoadFile;
+    $compatClassLoader = require __DIR__ . '/build/vendor/autoload.php';
+
+    \Helhum\Typo3Console\Core\Kernel::$nonComposerCompatClassLoader = $compatClassLoader;
+
+    return $classLoader;
+})();
