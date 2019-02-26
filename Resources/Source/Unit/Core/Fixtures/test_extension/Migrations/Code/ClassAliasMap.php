54dfb459b4be78c5855995510f54b7863115aac8
tudy <email@thomasruta.de>
2019-02-26 03:44:57 -0800

    [remove] leftovers from submodules

--- Resources/Source/Unit/Core/Fixtures/test_extension/Migrations/Code/ClassAliasMap.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'foo' => \TYPO3\CMS\Core\Tests\Unit\Core\Fixtures\test_extension\Resources\PHP\Test::class,
-    'bar' => \TYPO3\CMS\Core\Tests\Unit\Core\Fixtures\test_extension\Resources\PHP\AnotherTestFile::class,
-];
